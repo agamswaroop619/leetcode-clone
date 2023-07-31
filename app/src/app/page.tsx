2c13f3cbac0e1e607a660e49e2f322c55d6995ba
agamswaroop619 <agam.swaroop619@gmail.com>
2023-07-31 22:23:14 +0530

    Removing some errors

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,15 +2,15 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function App({component, pageprops}: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <head>
-      <title>Leetclone</title> 
-      <meta name='viewport' content='width-device-width, initial-scale=1' /> 
-      <link rel='icon' href='/favicon.png'/>
-      <meta name ='description' content='web application that contains leetcode problems' />
-    </head>
-  </>
+    <>
+      <head>
+        <title>Leetclone</title>
+        <meta name='viewport' content='width-device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.png' />
+        <meta name='description' content='web application that contains leetcode problems' />
+      </head>
+    </>
   )
 }
