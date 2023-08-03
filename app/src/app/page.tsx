8003a6b156b6c7f4e543bc9293cb54c4efcbfa8d
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-04 01:07:04 +0530

    Added metadata to layout

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,16 +1,10 @@
 // import "@/styles/global.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <head>
-        <title>Leetclone</title>
-        <meta name='viewport' content='width-device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.png' />
-        <meta name='description' content='web application that contains leetcode problems' />
-      </head>
+    Hello
     </>
   )
 }
