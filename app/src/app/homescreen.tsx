3b360a5600124b899efec764dcba67ea310eed61
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-13 12:56:51 +0530

    Homescreen screoll bug resolved!

--- app/src/app/homescreen.tsx
+++ app/src/app/homescreen.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 type Props = {}
 
 const Homescreen = (props: Props) => {
-  return (
-    <div className='relative h-[100vh] '>
+    
+    const navbarHeight = 4;
+    const availableHeight = `calc(100vh - ${navbarHeight}rem)`;
+    //Using TailwindCSS documentation
+    return (
+      <div className='relative' style={{ height: availableHeight }}>
         <div className='bg-red-500 absolute left-0 h-full w-[50%]'>
         </div>
         <div className='bg-yellow-500 absolute right-0 h-full w-[50%]'>
