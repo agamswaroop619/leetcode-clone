390fc26b9fa2a6650c28ac60f98d6b4b8ed2d456
agamswaroop619 <agam.swaroop619@gmail.com>
2023-08-16 17:15:41 +0530

    Login Screen updates

--- app/src/app/auth/Authmodel.tsx
+++ app/src/app/auth/Authmodel.tsx
@@ -11,12 +11,12 @@ export default function Authmodel({}: Props) {
   return (
 
     <div className='absolute bg-opacity-60 h- w-full bg-black flex items-centre justify-center z-10' style={{ height: availableHeight }}>
-      <div className='w-full sm:w-[450x] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center'>
+      <div className='w-full absolute top-[45%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center'>
         <div className='relative w-full h-full mx-auto flex items-center justify-center'>
-          <div className='bg-gradient-to-r from-black to-grey-900 rounded-full shadow relative w-[40%] bg-blue-600 Uto-slate-900 mx-6'>
+          <div className='bg-gradient-to-r from-black to-grey-900 rounded-full shadow relative w-[70%] sm:w-[60%] lg:w-[40%] bg-blue-600 Uto-slate-900 mx-6'>
             <div className='overflow-hidden'>
               <Link href='/'>
-              <button type='button' className='absolute bg-transparent rounded-1g text-sm px-5 py-2.5 ml-auto inline-flex items-center hover:text-gray-400 hover:font-bold text-white top-0 right-1' >
+              <button type='button' className='absolute bg-transparent rounded-1g text-xl px-5 py-5 ml-auto inline-flex items-center hover:text-gray-400 hover:font-bold text-white top-0 right-1' >
               X
               </button>
               </Link>
--- app/src/app/auth/login.tsx
+++ app/src/app/auth/login.tsx
@@ -4,11 +4,11 @@ type LoginProps = {}
 
 const Login = (props: LoginProps) => {
   return (
-    <div className='p-5' >
+    <div className='py-5 px-10' >
       <p className='text-2xl text-center font-bold font-mono'>Sign In</p>
-        <form>
-
-        <input></input>
+        <form className='font-mono italic p-2'>
+        <label className='p-2' htmlFor='email'>E-Mail</label>
+        <input className='w-full rounded-full focus:ring-white border-2 outline text-black p-1' type='email' name='email' id='email' placeholder='example_email@mail.org' ></input>
         </form>
     </div>
   )
